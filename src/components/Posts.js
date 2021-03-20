fcf6cb876ebf1077ad288cc89918187c0b23e0de
safin ghoghabori <safinghoghabori65@gmail.com>
2021-03-20 10:10:45 +0530

    changed post file

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import { Grid } from '@material-ui/core';
+import { Grid } from "@material-ui/core";
 
 function Posts(props) {
   const { _id, imageUrl } = props;
 
   //convert image url in proper formate
-  const imageUrlSplit = imageUrl.split('\\');
-  const finalImageUrl = `/${imageUrlSplit[0]}/${imageUrlSplit[1]}`;
+  // const imageUrlSplit = imageUrl.split('\\');
+  // const finalImageUrl = `/${imageUrlSplit[0]}/${imageUrlSplit[1]}`;
 
   return (
     <>
       <Grid item sm={4} xs={4}>
-        <img src={finalImageUrl} key={_id} alt="img3" className="postsImg" />
+        <img src={imageUrl} key={_id} alt="img3" className="postsImg" />
       </Grid>
     </>
   );
